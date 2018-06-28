ebcfa543e7ec4542cb7204271338701e672b60ad
ronald.arias <ronald.arias@udla.edu.ec>
2018-06-28 14:21:06 -0500

    Creacion del proyecto desde SpringBoot

---
+++ src/main/java/com/ronaldarias/apirest/DemoApirestApplication.java
@@ -0,0 +1,12 @@
+package com.ronaldarias.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApirestApplication.class, args);
+	}
+}
