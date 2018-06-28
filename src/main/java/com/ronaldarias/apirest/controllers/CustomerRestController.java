0c106d4c0c1312cf206330c21b19ea2566c417eb
ronald.arias <ronald.arias@udla.edu.ec>
2018-06-28 16:56:25 -0500

    Servicio POST para agregar un customer

--- src/main/java/com/ronaldarias/apirest/controllers/CustomerRestController.java
+++ src/main/java/com/ronaldarias/apirest/controllers/CustomerRestController.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ronaldarias.apirest.models.entity.Customer;
 import com.ronaldarias.apirest.models.service.CustomerService;
-
 
 @RestController
 @RequestMapping("/api")
@@ -31,6 +32,22 @@ public class CustomerRestController {
 		Customer customer = customerService.getCustomer(customerId);
 
 		return customer;
+	}
+
+	// add mapping for POST /customers - add new customer
+
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+
+		// also just in case the pass an id in JSON ... set id to 0
+		// this is force a save of new item ... instead of update
+
+		customer.setId(null);
+
+		customerService.saveCustomer(customer);
+
+		return customer;
+
 	}
 
 }
