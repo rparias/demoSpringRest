00b36fabf897fcee4bf9c78bc27e6f697a94ed3d
ronald.arias <ronald.arias@udla.edu.ec>
2018-06-29 11:11:36 -0500

    Agregar CrossOrigin al controlador para permitir el consumo de la API desde el navegador

--- src/main/java/com/ronaldarias/apirest/controllers/CustomerRestController.java
+++ src/main/java/com/ronaldarias/apirest/controllers/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.ronaldarias.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ronaldarias.apirest.models.entity.Customer;
 import com.ronaldarias.apirest.models.service.CustomerService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class CustomerRestController {
