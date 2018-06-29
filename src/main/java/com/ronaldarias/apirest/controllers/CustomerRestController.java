42c3f2a9b4c487f5d6c872c4c1303f44bef3cadb
ronald.arias <ronald.arias@udla.edu.ec>
2018-06-28 23:33:35 -0500

    REST para update(PUT) y para DELETE

--- src/main/java/com/ronaldarias/apirest/controllers/CustomerRestController.java
+++ src/main/java/com/ronaldarias/apirest/controllers/CustomerRestController.java
@@ -3,9 +3,11 @@ package com.ronaldarias.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,7 +49,26 @@ public class CustomerRestController {
 		customerService.saveCustomer(customer);
 
 		return customer;
-
+	}
+	
+	// add mapping for PUT /customers - update existing customer
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		
+		customerService.saveCustomer(customer);
+		
+		return customer;
+	}
+	
+	
+	//add mapping for DELETE /customers/{customerId} - delete existing customer
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		
+		customerService.deleteCustomer(customerId);
+		
+		return "Deleted customer id - " + customerId;
 	}
 
 }
